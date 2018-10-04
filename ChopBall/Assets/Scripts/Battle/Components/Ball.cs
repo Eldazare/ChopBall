5c3d9be6af65195719b27b12256024bf516b1938
Elda-LAPTOP <sakari.helokunnas@gmail.com>
2018-10-04 15:13:16 +0300

    Ball: preSpawnIndicator

--- ChopBall/Assets/Scripts/Battle/Components/Ball.cs
+++ ChopBall/Assets/Scripts/Battle/Components/Ball.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
 	public Vector3 startPosition; // initialize this with loader, use in Reset method
+	public GameObject preSpawnIndicator;
 	public List<Vector3> startPositions;
 	public List<int> touchedPlayers = new List<int>(16);
+
+	private GameObject preSpawnIndicatorInstance;
 
 
 	public void ResetBallPosition(){
@@ -19,6 +22,8 @@ public class Ball : MonoBehaviour {
 		foreach (var spawn in ballSpawns) {
 			startPositions.Add (spawn.position);
 		}
+		preSpawnIndicatorInstance = Instantiate (preSpawnIndicator, Vector3.zero, Quaternion.identity);
+		preSpawnIndicatorInstance.SetActive (false);
 	}
 
 	public void GetPlayerPaddleTouch(int playerID){
@@ -29,15 +34,20 @@ public class Ball : MonoBehaviour {
 	}
 
 	private IEnumerator ResetEnumerator(){
+		Vector3 spawnPos;
+		if (startPositions != null) {
+			spawnPos = startPositions [Random.Range (0, startPositions.Count)];
+		} else {
+			spawnPos = startPosition;
+		}
 		GetComponent<MeshRenderer> ().enabled = false;
 		GetComponent<CircleCollider2D> ().enabled = false;
 		GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		preSpawnIndicatorInstance.transform.position = spawnPos;
+		preSpawnIndicatorInstance.SetActive (true);
 		yield return new WaitForSeconds(3f);
-		if (startPositions != null) {
-			transform.position = startPositions [Random.Range (0, startPositions.Count)];
-		} else {
-			transform.position = startPosition;
-		}
+		preSpawnIndicatorInstance.SetActive(false);
+		transform.position = spawnPos;
 		GetComponent<MeshRenderer> ().enabled = true;
 		GetComponent<CircleCollider2D> ().enabled = true;
 	}

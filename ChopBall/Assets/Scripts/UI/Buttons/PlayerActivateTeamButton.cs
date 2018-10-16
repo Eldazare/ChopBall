9e56670c589f10600cb6a68ffa93607002300df7
Elda <sakari.helokunnas@gmail.com>
2018-10-16 13:40:47 +0300

    PlayerActivate buttons skeletons done, unfinished

---
+++ ChopBall/Assets/Scripts/UI/Buttons/PlayerActivateTeamButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerActivateTeamButton : _CursorButton {
+
+	public int playerID;
+
+	private PlayerStateData stateData;
+	private Text text;
+	private string baseStr;
+	private string teamPart;
+	private string characterChoicePart;
+
+	void Start(){
+		text = GetComponentInChildren<Text> ();
+		stateData = PlayerStateController.GetAState (playerID);
+		baseStr = "Player: " + playerID + "\n";
+		teamPart = "\n";
+		SetCharChoiceStr ();
+	}
+
+	// TODO: Check continous button input
+	public void GetInput(InputModel model){
+		if (model.PaddleRight) {
+			ChangeTeam (true);
+			SetTeamPart ();
+		}
+		if (model.PaddleLeft) {
+			ChangeTeam (false);
+			SetTeamPart ();
+		} if (model.Start) {
+			PlayerStateController.SetStateActive (playerID - 1);
+			GetComponent<Image>().enabled = true;
+			text.enabled = true;
+		}
+	}
+
+	private void ChangeTeam(bool incDec){
+		// TODO:
+	}
+
+	public void SetTeamPart(){
+		teamPart = "Team: " + stateData.team + "\n";
+		SetString ();
+	}
+
+	public void SetCharChoiceStr(){
+		if (stateData.characterChoice == -1) {
+			characterChoicePart = "Character not chosen.";
+		} else {
+			characterChoicePart = "Char choice: "+stateData.characterChoice;
+		}
+		characterChoicePart += "\n";
+		SetString ();
+	}
+
+	private void SetString(){
+		text.text = baseStr + teamPart + characterChoicePart;
+	}
+}

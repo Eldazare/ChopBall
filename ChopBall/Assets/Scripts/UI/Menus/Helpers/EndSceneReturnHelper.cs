12780f0bcf875a9c31f52a6af2e4d2763de50bee
Elda-LAPTOP <sakari.helokunnas@gmail.com>
2018-10-22 09:33:14 +0300

    Untested: Returning from MatchEndScreen

---
+++ ChopBall/Assets/Scripts/UI/Menus/Helpers/EndSceneReturnHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneReturnHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ReturnToMenu (5.0f);
+	}
+	
+	private IEnumerator ReturnToMenu(float delay){
+		Debug.Log ("Returning to menu in " + delay + " seconds...");
+		yield return new WaitForSeconds (delay);
+		SceneManager.LoadScene ("MenuScene");
+	}
+}

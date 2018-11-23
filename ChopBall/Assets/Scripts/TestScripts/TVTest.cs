df324e125d9adebfc0f59121657605419693d2ce
alluuuu <35914926+alluuuu@users.noreply.github.com>
2018-11-23 04:44:18 +0200

    Added camera and render texture for ingame screen

---
+++ ChopBall/Assets/Scripts/TestScripts/TVTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVTest : MonoBehaviour {
+
+    public GameObject Screen;
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        coroutine = Wait(15.0f);
+        StartCoroutine(coroutine);
+
+        Screen.SetActive(false);
+    }
+
+    private IEnumerator Wait(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Screen.SetActive(true);
+            yield return (new WaitForSeconds(5));
+            Screen.SetActive(false);
+        }
+    }
+}

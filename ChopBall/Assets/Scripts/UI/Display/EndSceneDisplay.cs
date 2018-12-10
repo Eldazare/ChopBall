5d40e3ff413ff941962f44c4e40944c1c29f6d94
Elda-LAPTOP <sakari.helokunnas@gmail.com>
2018-12-10 13:06:36 +0200

    Particles now only from correct goal | Adjusted end scene texts

--- ChopBall/Assets/Scripts/UI/Display/EndSceneDisplay.cs
+++ ChopBall/Assets/Scripts/UI/Display/EndSceneDisplay.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class EndSceneDisplay : MonoBehaviour {
 
+	public Text winnerText;
 	public List<Text> textList;
+	private int[] fontScales = { 90,75,65,65 };
 
 	private List<Text> displayTextList;
 
@@ -20,8 +22,14 @@ public class EndSceneDisplay : MonoBehaviour {
 				displayTextList [i].gameObject.SetActive (true);
 				displayTextList [i].color = pBaseData.teamColors [teams [i].teamID];
 				displayTextList [i].text = "Team " + teams [i].teamID + ":\n"
-				+ "Position: " + teams [i].endPosition + "\n"
+				+ "No. " + teams [i].endPosition + "\n"
 				+ "Score: " + teams [i].score + "\n";
+				if (teams [i].endPosition != 0) {
+					displayTextList [i].fontSize = fontScales [teams [i].endPosition - 1];
+				}
+				if (teams [i].endPosition == 1) {
+					winnerText.text = "Team " + teams [i].teamID + " is the winner!";
+				}
 			}
 		} else if (competitors != null) {
 			for (int i = 0; i < competitors.Count; i++) {
@@ -29,8 +37,14 @@ public class EndSceneDisplay : MonoBehaviour {
 				displayTextList [i].gameObject.SetActive (true);
 				displayTextList [i].color = pBaseData.playerColors [competitors [i].playerID - 1];
 				displayTextList [i].text = "Player " + competitors [i].playerID + ":\n"
-				+ "Position: " + competitors [i].endPosition + "\n"
+				+ "No. " + competitors [i].endPosition + "\n"
 				+ "Score: " + competitors [i].score + "\n";
+				if (competitors [i].endPosition != 0) {
+					displayTextList [i].fontSize = fontScales [competitors [i].endPosition - 1];
+				}
+				if (competitors [i].endPosition == 1) {
+					winnerText.text = "Player " + competitors [i].playerID + " is the winner!";
+				}
 			}
 		} else {
 			Debug.LogWarning ("No displaydata found.");
--- ChopBall/Assets/Scripts/UI/Menus/Helpers/EndSceneReturnHelper.cs
+++ ChopBall/Assets/Scripts/UI/Menus/Helpers/EndSceneReturnHelper.cs
@@ -5,14 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class EndSceneReturnHelper : MonoBehaviour {
 
-	// Use this for initialization
+
+	public GameObject endIndicator;
+	/*
 	void Start () {
 		StartCoroutine(ReturnToMenu (5.0f));
 	}
+	*/
 	
 	private IEnumerator ReturnToMenu(float delay){
 		Debug.Log ("Returning to menu in " + delay + " seconds...");
 		yield return new WaitForSeconds (delay);
 		SceneManager.LoadScene ("MenuScene2");
+	}
+
+
+	public void GetInput(InputModel model){
+		if (model.Start) {
+			endIndicator.SetActive (true);
+			SceneManager.LoadScene ("MenuScene2");
+		}
 	}
 }
